c7ad027de20dac4b181bff7de159d182cfd31c61
Shawn Kastner <Shawnkastner@hotmail.de>
2022-11-14 18:06:45 +0100

    function next image

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let dogImg = ['img/maja1.jpg', 'img/maja2.jpg', 'img/maja3.jpg', 'img/maja4.jpg', 'img/maja5.jpg', 'img/maja6.jpg', 'img/maja7.jpg', 'img/maja9.jpg', 'img/shiva&maja.jpg','img/shiva&maja2.jpg','img/shiva&maja3.jpg','img/shiva&maja7.jpg', 'img/shiva1.jpg', 'img/shiva2.jpg', 'img/shiva3.jpg', 'img/shiva4.jpg', 'img/shiva5.jpg', 'img/shiva6.jpg', 'img/shiva7.jpg','img/maja8.jpg', 'img/shiva8.jpg', 'img/shiva9.jpg', 'img/shiva10.jpg']
-
+let currentImage;
 
 function load() {
     let img = document.getElementById('imgContainer');
@@ -15,14 +15,16 @@ function load() {
 }
 
 function openImg(i) {
+    currentImage = i;
+
     document.getElementById('showBig').innerHTML =`
     <div class="arrows">
         <img onclick="left()" class="icon" src="icon/arrow-left.ico">
         <img onclick="closeImg()" class="icon" src="icon/x-mark-64.ico">
         <img onclick="right()" class="icon" src="icon/arrow-right.ico">
     </div>
-    <div class="bigImage">
-        <img class="fullImage" src="${dogImg[i]}">
+    <div id="container_bigImage" class="bigImage">
+        <img id="image" class="fullImage" src="${dogImg[i]}">
     </div>
     `;
 
@@ -33,4 +35,37 @@ function openImg(i) {
 function closeImg() {
     document.getElementById('showBig').classList.add('d-none');
 }
+
+function left() {
+    currentImage--;
+
+    if(currentImage < 0) {
+        currentImage = dogImg.length -1;
+    }
+
+    let imgBack = dogImg[currentImage];
+
+    document.getElementById('container_bigImage').innerHTML = `
+    <img class="fullImage" src="${imgBack}">
+    </div>
+    `;
+}
+
+function right() {
+    currentImage++;
+
+    if(currentImage == 0) {
+        currentImage = dogImg.length +1;
+    }
+    
+    let nextImage = dogImg[currentImage];
+
+    document.getElementById('container_bigImage').innerHTML = `
+    <img class="fullImage" src="${nextImage}">
+    </div>
+    `;
+}
+    
+
+
 
